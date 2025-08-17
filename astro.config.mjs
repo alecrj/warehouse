3d5ee2a691154a104cb87e7bd9f12be0fc88700b
alecrj <99alecrodriguez@gmail.com>
2025-08-17 15:22:34 -0400

    Fix forms and add lead management

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify';
+
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'hybrid', // Changed from 'server'
+  output: 'hybrid',
   adapter: netlify({
     edgeMiddleware: false
   }),
